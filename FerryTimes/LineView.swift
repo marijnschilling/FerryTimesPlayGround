462d78e711562fdfbee21c2c123f3a8f84340afa
marijnschilling <marijnschilling@gmail.com>
2017-10-01 21:41:44 +0200

    Change project structure

--- FerryTimes/LineView.swift
+++ FerryTimes/LineView.swift
@@ -4,34 +4,45 @@
 //
 
 import UIKit
+import TinyConstraints
+import SweetUIKit
 
 class LineView: UIView {
     fileprivate lazy var fromTextLabel: UILabel = {
-        let fromTextLabel = UILabel()
+        let fromTextLabel = UILabel(withAutoLayout: true)
+        fromTextLabel.font = .preferredFont(forTextStyle: .body)
+        fromTextLabel.text = "Van"
 
         return fromTextLabel
     }()
 
     fileprivate lazy var fromStationLabel: UILabel = {
         let fromStationLabel = UILabel()
+        fromStationLabel.font = .preferredFont(forTextStyle: .title1)
+        fromStationLabel.text = "Amsterdam Centraal"
 
         return fromStationLabel
     }()
 
     fileprivate lazy var toTextLabel: UILabel = {
         let toTextLabel = UILabel()
+        toTextLabel.font = .preferredFont(forTextStyle: .body)
+        toTextLabel.text = "Van"
 
         return toTextLabel
     }()
 
     fileprivate lazy var toStationLabel: UILabel = {
         let toStationLabel = UILabel()
+        toStationLabel.font = .preferredFont(forTextStyle: .title1)
+        toStationLabel.text = "Buiksloterweg"
 
         return toStationLabel
     }()
 
     fileprivate lazy var switchButton: UIButton = {
         let switchButton = UIButton()
+        switchButton.backgroundColor = .white
 
         return switchButton
     }()
@@ -47,10 +58,21 @@ class LineView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
 
+        backgroundColor = .blue
+
         addSubviewsAndConstraints()
     }
 
     private func addSubviewsAndConstraints() {
-        
+        addSubview(toTextLabel)
+        addSubview(toStationLabel)
+
+        addSubview(fromTextLabel)
+        addSubview(fromStationLabel)
+
+        addSubview(switchButton)
+
+        toTextLabel.top(to:)
+
     }
 }

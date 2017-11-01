b328013c2688f798392850ab4bc905fd635141c5
marijnschilling <marijnschilling@gmail.com>
2017-11-01 22:23:29 +0100

    Sets some constraints and pointsizes

--- FerryTimes/iOS/Source/Views/LineView.swift
+++ FerryTimes/iOS/Source/Views/LineView.swift
@@ -11,16 +11,17 @@ import QuartzCore
 class LineView: UIView {
     private lazy var fromTextLabel: UILabel = {
         let fromTextLabel = UILabel(withAutoLayout: true)
-        fromTextLabel.font = .preferredFont(forTextStyle: .body)
+        fromTextLabel.font = UIFontMetrics(forTextStyle: .body).scaledFont(for: .systemFont(ofSize: 16), maximumPointSize: 34)
         fromTextLabel.text = "Van"
         fromTextLabel.textColor = Theme.lightTextColor
+        fromTextLabel.adjustsFontForContentSizeCategory = true
 
         return fromTextLabel
     }()
 
     private lazy var fromStationLabel: UILabel = {
         let fromStationLabel = UILabel()
-        fromStationLabel.font = UIFontMetrics(forTextStyle: .title1).scaledFont(for: .systemFont(ofSize: 28), maximumPointSize: 60)
+        fromStationLabel.font = UIFontMetrics(forTextStyle: .title1).scaledFont(for: .systemFont(ofSize: 28), maximumPointSize: 50)
         fromStationLabel.text = "Centraal Station"
         fromStationLabel.textColor = Theme.lightTextColor
         fromStationLabel.adjustsFontForContentSizeCategory = true
@@ -30,7 +31,7 @@ class LineView: UIView {
 
     private lazy var toTextLabel: UILabel = {
         let toTextLabel = UILabel()
-        toTextLabel.font = .preferredFont(forTextStyle: .body)
+        toTextLabel.font = UIFontMetrics(forTextStyle: .body).scaledFont(for: .systemFont(ofSize: 16), maximumPointSize: 34)
         toTextLabel.text = "Naar"
         toTextLabel.textColor = Theme.lightTextColor
         toTextLabel.adjustsFontForContentSizeCategory = true
@@ -40,7 +41,7 @@ class LineView: UIView {
 
     private lazy var toStationLabel: UILabel = {
         let toStationLabel = UILabel()
-        toStationLabel.font = UIFontMetrics(forTextStyle: .title1).scaledFont(for: .systemFont(ofSize: 28), maximumPointSize: 60)
+        toStationLabel.font = UIFontMetrics(forTextStyle: .title1).scaledFont(for: .systemFont(ofSize: 28), maximumPointSize: 50)
         toStationLabel.text = "Buiksloterweg"
         toStationLabel.textColor = Theme.lightTextColor
         toStationLabel.adjustsFontForContentSizeCategory = true
@@ -75,21 +76,23 @@ class LineView: UIView {
         let buttonSize: CGFloat = 48.0 
         let margin: CGFloat = 24.0
 
-        addSubview(toTextLabel)
-        addSubview(toStationLabel)
-
         addSubview(fromTextLabel)
         addSubview(fromStationLabel)
+
+        addSubview(toTextLabel)
+        addSubview(toStationLabel)
 
         addSubview(switchButton)
 
         fromTextLabel.top(to: self, offset: 38)
-        fromTextLabel.height(21, relation: .equalOrGreater)
+        fromTextLabel.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+        fromTextLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
         fromTextLabel.left(to: self, offset: margin)
         fromTextLabel.right(to: self, offset: -margin, relation: .equalOrGreater)
 
         fromStationLabel.topToBottom(of: fromTextLabel, offset: 5)
         fromStationLabel.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+        fromStationLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
         fromStationLabel.left(to: self, offset: margin)
         fromStationLabel.right(to: self, offset: -margin, relation: .equalOrGreater)
 
@@ -102,13 +105,15 @@ class LineView: UIView {
 
         toTextLabel.topToBottom(of: fromStationLabel, offset: margin)
         toTextLabel.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+        toTextLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
         toTextLabel.left(to: self, offset: margin)
         toTextLabel.right(to: self, offset: -margin, relation: .equalOrGreater)
 
         toStationLabel.topToBottom(of: toTextLabel, offset: 5)
         toStationLabel.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+        toStationLabel.setContentHuggingPriority(.defaultLow, for: .vertical)
         toStationLabel.left(to: self, offset: margin)
         toStationLabel.right(to: self, offset: -margin, relation: .equalOrGreater)
-        toStationLabel.bottom(to: self, offset: -margin)
+        toStationLabel.bottom(to: self, offset: -margin, relation: .equalOrGreater)
     }
 }
